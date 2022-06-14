ffa70853ede8c426faff7f46f8c40497bd56feb0
Wisanu <wisanu.pho@cru.ac.th>
2022-06-14 13:01:50 +0700

    usestate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React,{useState} from 'react';
 import FirstComponent from './components/FirstComponent';
 import SecondComponent from './components/SecondComponent';
 
 const App = () => {
-  var count = 0;
+  const [count, setCount] = useState(0)
   const name = 'Wisanu';
   const surName = 'Phornjariyatham';
   const bool = true;
@@ -17,8 +17,8 @@ const App = () => {
       {bool ? <h1>TRUE</h1> : <h1>FALSE</h1>}
       <h3>{count}</h3>
       <button
-        onClick={() => {
-          count = count + 1;
+        onClick={() => {setCount(count+1)
+          
         }}
       >
         ADD
